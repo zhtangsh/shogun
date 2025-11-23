16bb12c64a8dade106a81dd9dbb2724077327dcc
Zihan Eric Tang <mrzihan.tang@gmail.com>
2025-11-23 14:38:41 +0800

    feat: :sparkles: add build script for dzqh

---
+++ build_dzqh.sh
@@ -0,0 +1,6 @@
+VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]')
+SHORT_SHA=$(git rev-parse --short HEAD)
+yarn build
+echo "build image 192.168.20.204:30000/wdl/shogun:$VERSION.$SHORT_SHA"
+docker build . -t 192.168.20.204:30000/wdl/shogun:$VERSION.$SHORT_SHA
+docker push 192.168.20.204:30000/wdl/shogun:$VERSION.$SHORT_SHA
